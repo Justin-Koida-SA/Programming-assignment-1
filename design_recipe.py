64a639b431ec37bd8918e146ed83e4a70056ab47
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2023-02-15 16:49:19 -0800

    Create design_recipe.py

---
+++ design_recipe.py
@@ -0,0 +1,64 @@
+#Part 1
+"""
+For part one, my function is called vowel_count. This function takes in a string input, then returns
+the number of vowels in the string. This function returns an integer
+"""
+
+#Part 2
+"""
+For part two, my function short_lists takes in one parameter of the nested list type. The purpose of this function is 
+to create a new list containing elements of the input list of length 2. The output of this function is another nested
+list with elements of length 2.
+"""
+
+#Part 3
+"""
+For part three, my function ascending_pairs takes in one parameter of the nested list type. The output of this function
+is another nested list. This function goes through the input nested list, and for every element of length 2, it puts
+them in ascending order. Then it puts each element (regardless of length) in a new list. 
+"""
+
+#Part 4
+
+"""
+For part four, my function add_prices simply adds two given prices. This function makes sure the cents in the added 
+prices do not go over 100.This function takes in 2 parameters each of the 
+data type price. It outputs the two prices added together, thus it returns the data type price. 
+"""
+
+#Part 5
+
+"""
+For part five, my function rectangle_area finds the area of a rectangle. The function takes 1 input
+of data type Rectangle and its output is the area of that given rectangle. The data Rectangle by default takes
+2 inputs of type Point.
+"""
+
+#Part 6
+
+"""
+For part six, my function books_by_author takes in 2 inputs, the first a string, and the second a list. The purpose of
+this function is to go through the input list (which are suppose to be book titles with authors) and return each book
+title from a given author which is the input string of the function. It's output is a list of book titles
+"""
+
+#Part 7
+"""
+For part seven, my function circle_bound takes 1 input of data type Rectangle. This function's purpose is to 
+create the smallest possible circle that encloses the given rectangle. The output of this function is of data type 
+Circle.
+"""
+
+#Part 8
+
+"""
+For part eight, my function below_pay_average takes one input list of data type Employee and its output returns a list  
+of strings. The purpose of this function is to find all the people in the input list that are given a pay below
+the average of the given list.
+"""
+
+
+
+
+
+
